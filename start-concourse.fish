5032e78f9edb3c8083d0003632d292a4b4e0ac39
shinyay <shinya.com@gmail.com>
2019-02-04 17:39:18 +0900

    Add start operation with usting quick template

--- start-concourse.fish
+++ start-concourse.fish
@@ -1,6 +1,10 @@
 #!/usr/bin/env fish
-./generate-keys.sh
-docker-compose up -d
+
+#./generate-keys.sh
 
 set URL (ipconfig getifaddr en0)
 set -x CONCOURSE_EXTERNAL_URL http://$URL:8080
+
+docker-compose -f docker-compose-quickstart.yml up -d
+
+echo $CONCOURSE_EXTERNAL_URL
